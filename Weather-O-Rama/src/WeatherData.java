fdc9763dbe48d7e7b66f4363f41f3acbd0df5934
Nils <Ghesquiere.nils@gmail.com>
2015-10-30 13:33:24 +0100

    Class WeatherData added

---
+++ Weather-O-Rama/src/WeatherData.java
@@ -0,0 +1,55 @@
+
+import java.util.ArrayList;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author Nils
+ */
+public class WeatherData implements Subject {
+
+    //Generics gebruikt voor de ArrayList zodat we niet meer moeten typecasten
+    private final ArrayList<Observer> observers;
+    private float temperature;
+    private float humidity;
+    private float pressure;
+
+    public WeatherData() {
+        observers = new ArrayList<>();
+    }
+
+    @Override
+    public void registerObserver(Observer o) {
+        observers.add(o);
+    }
+
+    @Override
+    public void removeObserver(Observer o) {
+        int i = observers.indexOf(o);
+        if (i >= 0) {
+            observers.remove(i);
+        }
+    }
+
+    @Override
+    public void notifyObservers() {
+        //opgelost met lambda inplaats van voorbeeld in boek
+        observers.stream().forEach(observer -> observer.update(temperature, humidity, pressure));
+    }
+
+    public void measurementsChanged() {
+        notifyObservers();
+    }
+
+    public void setMeasurements(float temperature, float humidity, float pressure) {
+        this.temperature = temperature;
+        this.humidity = humidity;
+        this.pressure = pressure;
+        measurementsChanged();
+    }
+    //other Weatherdata methods here
+}
