abe36a2e35fece8b1e77fe34d9f59ced52be5ddc
Nils <Ghesquiere.nils@gmail.com>
2015-10-30 15:31:27 +0100

    Made the example

--- Observer Design Pattern/SwingObserverExample/src/swingobserverexample/SwingObserverExample.java
+++ Observer Design Pattern/SwingObserverExample/src/swingobserverexample/SwingObserverExample.java
@@ -5,17 +5,55 @@
  */
 package swingobserverexample;
 
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
 /**
  *
  * @author Nils
  */
 public class SwingObserverExample {
 
+    JFrame frame;
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        SwingObserverExample example = new SwingObserverExample();
+        example.go();
     }
-    
+
+    public void go() {
+        frame = new JFrame();
+        JButton button = new JButton("Should I do it?");
+        button.addActionListener(new AngelListener());
+        button.addActionListener(new DevilListener());
+        frame.getContentPane().add(BorderLayout.CENTER, button);
+        //set frame properties here
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().add(BorderLayout.CENTER, button);
+        frame.setSize(300, 300);
+        frame.setVisible(true);
+    }
+
+    class AngelListener implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent ae) {
+            System.out.println("Don't do it, you might regret it!");
+        }
+    }
+
+    class DevilListener implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent ae) {
+            System.out.println("Come on, do it!");
+        }
+    }
+
 }
