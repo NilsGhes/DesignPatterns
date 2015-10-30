90b17ba8f0bd1bf0506bef0ae2338fb343e57142
Nils <Ghesquiere.nils@gmail.com>
2015-10-30 11:39:42 +0100

    Class ModelDuck added

---
+++ SimUDuck/src/ModelDuck.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Nils
+ */
+public class ModelDuck extends Duck {
+
+    public ModelDuck() {
+        this.flyBehavior = new FlyNoWay();
+        this.quackBehavior = new Quack();
+    }
+
+    @Override
+    public void display() {
+        System.out.println("I'm a model duck");
+    }
+
+}
