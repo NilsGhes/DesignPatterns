5358d80735b0958b109ed1ee3b21ad768195f794
Nils <Ghesquiere.nils@gmail.com>
2015-11-03 09:36:25 +0100

    Condiments Created

---
+++ Decorator Design Pattern/Starbuzz/src/Mocha.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Nils
+ */
+public class Mocha extends CondimentDecorator {
+    Beverage beverage;
+    
+    public Mocha(Beverage beverage){
+        this.beverage = beverage;
+    }
+    
+    @Override
+    public String getDescription() {
+        return beverage.getDescription() + ", Mocha";
+    }
+
+    @Override
+    public double cost() {
+        return .20 + beverage.cost();
+    }
+    
+}
