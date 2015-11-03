e9facc4d5ad28b50298bfbf5ebf347dede376d7f
Nils <Ghesquiere.nils@gmail.com>
2015-11-03 10:09:30 +0100

    Finished project

--- Decorator Design Pattern/LowerCaseInputStream/src/InputTest.java
+++ Decorator Design Pattern/LowerCaseInputStream/src/InputTest.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package lowercaseinputstream;
+
 
 import java.io.BufferedInputStream;
 import java.io.FileInputStream;
