784509df2bea0ed86733c80fa386af6520b51015
Nils <Ghesquiere.nils@gmail.com>
2015-11-03 11:02:11 +0100

    PizzaStore copied

---
+++ Factory Design Pattern/PizzaStoreFactoryPattern/src/pizzastore/Pizza.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pizzastore;
+
+/**
+ *
+ * @author Nils
+ */
+public abstract class Pizza {
+    
+    public void prepare(){};
+    public void bake(){};
+    public void cut(){};
+    public void box(){};
+}
