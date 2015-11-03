9721631f22f675e161ec6f2f23a277cfbecd45c6
Nils <Ghesquiere.nils@gmail.com>
2015-11-03 10:06:02 +0100

    Added InputTest.java

---
+++ Decorator Design Pattern/LowerCaseInputStream/src/lowercaseinputstream/InputTest.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lowercaseinputstream;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ *
+ * @author Nils
+ */
+public class InputTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int c;
+
+        try (InputStream in = new LowerCaseInputStream(new BufferedInputStream(new FileInputStream("test.txt")))) {
+            while((c=in.read()) >=0){
+                System.out.print((char)c);
+            }
+        } catch (IOException e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+}
--- Decorator Design Pattern/LowerCaseInputStream/src/lowercaseinputstream/LowerCaseInputStream.java
+++ Decorator Design Pattern/LowerCaseInputStream/src/lowercaseinputstream/LowerCaseInputStream.java
@@ -5,17 +5,32 @@
  */
 package lowercaseinputstream;
 
+import java.io.FilterInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
 /**
  *
  * @author Nils
  */
-public class LowerCaseInputStream {
+public class LowerCaseInputStream extends FilterInputStream {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public LowerCaseInputStream(InputStream in) {
+        super(in);
     }
     
+    @Override
+    public int read() throws IOException{
+        int c = super.read();
+        return (c==-1 ? c: Character.toLowerCase((char)c));
+    }
+
+    @Override
+    public int read(byte[] b, int offset, int len) throws IOException{
+        int result = super.read(b,offset,len);
+        for (int i = offset;i<offset+result;i++){
+            b[i] = (byte)Character.toLowerCase((char)b[i]);
+        }
+        return result;
+    }
 }
